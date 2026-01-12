8bfe1a95327c64759c5844b1e6002b3cfa783cc7
Siddharth Ravani <sidd.ravani@gmail.com>
2026-01-12 10:59:43 +0530

    Fixed resume download error

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Link } from "wouter";
 import { Button } from "@/components/ui/button";
 import { TypewriterEffect } from "@/components/ui/typewriter-effect";
 import sidpic from "@/assets/Sid_ravani.jpeg"
-import resume from "@/assets/Sid-ravani.pdf"
+import resume from "@/assets/Sid-Resume.pdf"
 
 export default function Home() {
   const skills = [
